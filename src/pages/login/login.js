556bde6204c1b941cc29ce7208e0b11bd5b151cf
SeeJia <seejiajia1010@gmail.com>
2023-07-06 17:21:39 +0800

    code the login page

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -5,7 +5,7 @@ const Login = () => {
     const [password, setPassword] = useState(11223344);
     
     const sendLoginForMock = async () => {
-    const response = await axios.post('',{
+    const response = await axios.post('https://testing123.free.mockoapp.net',{
         username,
         password,
     });
@@ -15,7 +15,33 @@ const Login = () => {
    };
 
    return (
-    <div></div>
+    <div>
+        <h1>Login Page</h1>
+        <form onSubmit={sendLoginForMock}>
+            <div>
+            <label htmlFor="username">Username:</label>
+                <input
+                    type="text"
+                    id="username"
+                    value={username}
+                    onChange={() => setUsername(e.target.value)}
+                />
+            </div>
+
+            <div>
+            <label htmlFor="password">Password:</label>
+                <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={() => setPassword(e.target.value)}
+                />
+        </div>
+
+        <button type="submit">Login</button>
+        
+        </form>
+    </div>
   )
 }
 
