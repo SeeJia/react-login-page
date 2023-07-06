a3b4a48f99433cb7ed2776d57672ea724e7e5820
SeeJia <seejiajia1010@gmail.com>
2023-07-06 17:16:33 +0800

    setting mock by using axios

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
-  return (
-    <div>login</div>
+    const [username, setUsername] = useState('zhangsoon');
+    const [password, setPassword] = useState(11223344);
+    
+    const sendLoginForMock = async () => {
+    const response = await axios.post('',{
+        username,
+        password,
+    });
+
+    console.log(response.data);
+   
+   };
+
+   return (
+    <div></div>
   )
 }
 
